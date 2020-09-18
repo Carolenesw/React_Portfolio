a4efc441dff092e74aa4ba1945a6f0cc1e35b1e7
Carolene Wilson <carolenesw@gmail.com>
2020-09-18 12:10:33 -0400

    style main content

--- src/components/pages/main.js
+++ src/components/pages/main.js
@@ -6,21 +6,47 @@ import { Grid, Cell } from "react-mdl";
 class Main extends Component {
   render() {
     return (
-      <div style={{width: "100%", margin: "auto"}}>
+      <div style={{ width: "100%", margin: "auto" }}>
         <Grid className="landing">
-        <Cell col={12}>
-        <img 
-        src={require("../image/profile_pic.jpg")} alt="profile_pic" className="profile_img"
-        />
+          <Cell col={12}>
+            <img
+              src={require("../image/profile_pic.jpg")}
+              alt="profile_pic"
+              className="profile_img"
+            />
+            <div className="banner">
+              <h1>Carolene Wilson-Grizzle</h1>
 
-        </Cell>
+              <hr />
 
+              <p>
+                Python | HTLM/CSS | JavaScript | React| Express| Nodejs | MonoDB
+              </p>
+              <div className="socialMediaLinks">
+                {/* linkedin */}
+                <a
+                  href="https://www.linkedin.com/in/carolene-wilson-grizzle-8121bb82/"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                {/* Github */}
+                <a
+                  href="https://github.com/Carolenesw"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+                
+              </div>
+            </div>
+          </Cell>
         </Grid>
       </div>
     );
   }
 }
-
-
 
 export default Main;
